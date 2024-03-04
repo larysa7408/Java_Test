09f466977b77e26d71c97a0ba340adf02dacfc43
Larysa Petrova <158000446+larysa7408@users.noreply.github.com>
2024-03-04 18:24:32 +0100

    Commit

---
+++ src/main/java/homework_21/RubberArray.java
@@ -0,0 +1,85 @@
+package homework_21;
+
+public class RubberArray {
+    private final double RESIZE_KOEF = 1.5;
+    private final int INIT_DATA_SIZE = 10;
+    private int[] data;
+    private int length;
+
+    public RubberArray() {
+        data = new int[INIT_DATA_SIZE];
+        length = 0;
+    }
+
+    // Получение значения по индексу
+    public int get(int idx) {
+        return data[idx];
+    }
+
+    private void extendArrayIfNeed() {
+        if (length == data.length) {
+            // Создаем новый массив увеличенного размера на 1.5
+            int[] newData = new int[(int) (data.length * RESIZE_KOEF)];
+            // Переносим все элементы в новый массив
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            // Меняем ссылку на новый массив
+            data = newData;
+        }
+    }
+
+    // Добавление значения в конец массива
+    public void add(int value) {
+        extendArrayIfNeed();
+        data[length] = value;
+        length++;
+    }
+
+    // Добавление значения в указанную позицию массива
+    public void add(int value, int idx) {
+        extendArrayIfNeed();
+        for (int i = length; i > idx; i--) {
+            data[i] = data[i - 1];
+        }
+        data[idx] = value;
+        length++;
+    }
+
+    // Удаление элемента по индексу
+    public void remove(int idx) {
+        // Смещаем элементы справа налево, начиная с индекса idx
+        for (int i = idx; i < length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        length--;
+    }
+
+    // Проверка наличия значения в массиве
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
+    }
+
+    // Получение индекса значения в массиве
+    public int indexOf(int value) {
+        for (int i = 0; i < length; i++) {
+            if (data[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < length; i++) {
+            sb.append(data[i]);
+            if (i < length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/test/java/homework_21/RubberArrayTest.java
@@ -0,0 +1,65 @@
+package homework_21;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ AIT-TR, cohort 42.1, Java Basic, hm # 21
+
+ @author Larysa Buga
+ @version 03-Mar-2024
+ */
+
+public class RubberArrayTest {
+    @Test
+    public void testAddGet(){
+        RubberArray rubberArray=new RubberArray();
+        rubberArray.add(1);
+        rubberArray.add(2);
+        rubberArray.add(3);
+        Assertions.assertEquals(1,rubberArray.get(0));
+        Assertions.assertEquals(2,rubberArray.get(1));
+        Assertions.assertEquals(3,rubberArray.get(2));
+    }
+    @Test
+    public void testAddAtIndex(){
+        RubberArray rubberArray= new RubberArray();
+        rubberArray.add(1);
+        rubberArray.add(3);
+        rubberArray.add(5);
+        rubberArray.add(1,1);
+        Assertions.assertEquals(2,rubberArray.get(1));
+        Assertions.assertEquals(3,rubberArray.get(2));
+
+
+
+    }
+    @Test
+    public void testRemove(){
+        RubberArray rubberArray=new RubberArray();
+        rubberArray.add(1);
+        rubberArray.add(2);
+        rubberArray.add(3);
+        rubberArray.remove(1);
+        Assertions.assertEquals(1,rubberArray.get(0));
+        Assertions.assertEquals(3,rubberArray.get(1));
+    }
+    @Test
+    public void testContains(){
+        RubberArray rubberArray=new RubberArray();
+        rubberArray.add(1);
+        rubberArray.add(2);
+        rubberArray.add(3);
+        Assertions.assertTrue(rubberArray.contains(2));
+        Assertions.assertFalse(rubberArray.contains(4));
+    }
+    @Test
+    public void testIndexOf(){
+        RubberArray rubberArray=new RubberArray();
+        rubberArray.add(1);
+        rubberArray.add(2);
+        rubberArray.add(3);
+        Assertions.assertEquals(1,rubberArray.indexOf(2));
+        Assertions.assertEquals(-1,rubberArray.indexOf(4));
+    }
+}
